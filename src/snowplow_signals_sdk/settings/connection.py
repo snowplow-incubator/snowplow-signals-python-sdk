b0878597d609ed71c24d5047775e9b625bf4ad38
Jack Keene <jack.keene@snowplowanalytics.com>
2025-02-13 17:21:27 +0000

    integrate with signals api

--- src/snowplow_signals_sdk/settings/connection.py
+++ src/snowplow_signals_sdk/settings/connection.py
@@ -4,9 +4,8 @@ from pydantic import BaseModel
 
 
 class ConnectionSettings(BaseModel):
-    BDP_ORG_ID: str = os.environ.get("BDP_ORG_ID")
     BPD_CONSOLE_API_URL: str = (
-        "localhost:8000/api/v1/"  # Will change depending on where API is hosted
+        "http://localhost:8000/api/v1"  # Will change depending on where API is hosted
     )
 
 
--- src/snowplow_signals_sdk/signals_store.py
+++ src/snowplow_signals_sdk/signals_store.py
@@ -1,52 +1,76 @@
-from enum import Enum
-from typing import Optional, Union
+from typing import Any, Literal, Optional, Union
 
 from pydantic import BaseModel
 
-from .api_client.api_client import DEFAULT_API_CLIENT, ApiClient
-from .models.browser_features import BrowserFeatures
-from .models.feature_response import FeatureResponse
-from .models.feature_service import FeatureService
-from .models.feature_view import FeatureView
-from .utils import make_post_request
+from .api_client import DEFAULT_API_CLIENT, ApiClient
+from .models.feast.base_feast_object import BaseFeastObject
+from .models.feast.feature_service import FeatureService
+from .models.feast.feature_view import FeatureView
+from .models.online_feature_response import OnlineFeatureResponse
+
+
+class ApplyResponse(BaseModel):
+    status: Literal["applied", "nothing to apply"]
+
+
+class GetOnlineFeaturesRequest(BaseModel):
+    entities: dict[str, list[Any]]
+    feature_service: str | None = None
+    features: list[str] | None = None
+    full_feature_names: bool = False
 
 
 class SignalsStore(BaseModel):
     """Interface to interact with Snowplow Signals AI"""
 
-    browser_features: Optional[BrowserFeatures] = None
     api_client: ApiClient = DEFAULT_API_CLIENT
+
+    def apply(self, objects: Optional[list[BaseFeastObject]] = None) -> "ApplyResponse":
+        if objects:
+            for object in objects:
+                object.register_to_store(api_client=self.api_client)
+
+        response = self.api_client.make_post_request(
+            endpoint="feature_store/apply",
+        )
+        return ApplyResponse(**response)
 
     def get_online_features(
         self,
         features: Union[FeatureService, list[FeatureView]],
         entity: Optional[str] = None,
         entity_type_id: str = "domain_userid",
-    ) -> FeatureResponse:
+    ) -> Optional[OnlineFeatureResponse]:
 
-        entity = entity or getattr(self.browser_features, entity_type_id, None)
         if entity is None:
             raise ValueError(f"Entity `{entity_type_id}` could not be determined.")
 
         if not features:
-            return None
+            raise ValueError(f"No Features to fetch.")
 
         if isinstance(features, FeatureService):
-            data = {
-                "feature_service": features.name,
-                "entities": {entity_type_id: [entity]},
-            }
-        elif isinstance(features, list[FeatureView]):
-            data = {
-                "features": [f"{f.feature_view_name}:{f.name}" for f in features],
-                "entities": {entity_type_id: [entity]},
-            }
+            data = GetOnlineFeaturesRequest(
+                feature_service=features.name,
+                entities={entity_type_id: [entity]},
+            )
+
+        elif all(isinstance(fv, FeatureView) for fv in features):
+            feature_names = [
+                f"{fv.feast_name}:{feature.name}"
+                for fv in features
+                for feature in fv.features
+            ]
+            data = GetOnlineFeaturesRequest(
+                features=feature_names,
+                entities={entity_type_id: [entity]},
+                full_feature_names=True,
+            )
         else:
             raise TypeError(
                 "Features must be a FeatureService or a list of FeatureView."
             )
 
         response = self.api_client.make_post_request(
-            endpoint="/get-online-features/", data=data
+            endpoint="get-online-features", data=data.model_dump(mode="json")
         )
-        return FeatureResponse(**response) if response else None
+        return OnlineFeatureResponse(**response) if response else None
