9a426e3a79da22839ba3b3453ba4d3a5b38e296c
Jack Keene <jack.keene@snowplowanalytics.com>
2025-02-20 11:43:20 +0000

    export exposed types

---
+++ src/snowplow_signals/__init__.py
@@ -0,0 +1,29 @@
+from snowplow_signals.models.data_source import DataSource
+from snowplow_signals.models.entity import (
+    Entity,
+    session_entity,
+    user_entity,
+)
+from snowplow_signals.models.feature import (
+    Feature,
+    FilterCombinator,
+    FilterCondition,
+)
+from snowplow_signals.models.feature_service import FeatureService
+from snowplow_signals.models.feature_view import FeatureView
+from snowplow_signals.models.field import Field
+from snowplow_signals.signals import Signals
+
+Signals
+FeatureView
+FeatureService
+Feature
+Field
+FilterCombinator
+FilterCondition
+DataSource
+Entity
+user_entity
+session_entity
+
+# TODO: this is incomplete, add all published classes
