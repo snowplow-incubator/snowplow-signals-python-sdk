cc308069de365af7490dcf05e9cfad8d6d8abdf2
Matúš Tomlein <matus.tomlein@gmail.com>
2025-03-28 15:57:45 +0100

    Fix failing test for registry client

--- test/test_registry_client.py
+++ test/test_registry_client.py
@@ -1,15 +1,26 @@
 import httpx
+import pytest
 from datetime import timedelta
 
 from snowplow_signals import View, user_entity, LinkEntity, Attribute, Event
 from snowplow_signals.models import ViewOutput
 from snowplow_signals.api_client import ApiClient
 from snowplow_signals.registry_client import RegistryClient
+from .utils import MOCK_ORG_ID
 
 
 class TestRegistryClient:
 
-    def test_serializes_period_correctly_using_iso_format(self, respx_mock):
+    @pytest.fixture
+    def api_client(self):
+        return ApiClient(
+            api_url="http://localhost:8000",
+            api_key="foo",
+            api_key_id="bar",
+            org_id=MOCK_ORG_ID,
+        )
+
+    def test_serializes_period_correctly_using_iso_format(self, respx_mock, api_client):
         view = View(
             name="my_view",
             entity=user_entity,
@@ -42,7 +53,6 @@ class TestRegistryClient:
             "http://localhost:8000/api/v1/registry/views/"
         ).mock(return_value=httpx.Response(201, json=view_output.model_dump()))
 
-        api_client = ApiClient(api_url="http://localhost:8000")
         registry_client = RegistryClient(api_client=api_client)
         registry_client.apply([view])
 
