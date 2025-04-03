722b5c7c6279db410ca8dbb85ce29e8794d4acbc
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2025-04-03 12:40:51 +0300

    push changes done on tests
    * changes connection-test cli logic
    * revamped the tests a little bit

--- test/utils.py
+++ test/utils.py
@@ -1,6 +1,11 @@
 from datetime import UTC, datetime
 
 MOCK_ORG_ID = "1111-1111-1111-1111-1111"
+MOCK_API_URL = "http://localhost:8087"
+MOCK_API_KEY = "test-api-key"
+MOCK_API_KEY_ID = "test-key-id"
+MOCK_VIEW_NAME = "test_view"
+MOCK_VIEW_VERSION = 1
 
 
 def utc_timestamp():
