c776b210d044e5cff525f10696d789e657a03cbf
Agnes Kiss <agnes@snowplowanalytics.com>
2025-04-07 20:03:15 +0100

    Fix count and sum calculations

--- src/snowplow_signals/batch_autogen/models/dbt_config_generator.py
+++ src/snowplow_signals/batch_autogen/models/dbt_config_generator.py
@@ -226,7 +226,8 @@ class DbtConfigGenerator(BaseModel):
                                 if step["aggregation"] == "count":
                                     condition_clause = f"case when {condition_statement} then 1 else 0 end"
                                 elif step["aggregation"] == "sum":
-                                    condition_clause = f"case when {condition_statement} then cast({step['column_name']} as {{ dbt.type_float()}}) else 0 end"
+                                    property_name = attribute[0].get("column_name")
+                                    condition_clause = f"case when {condition_statement} then cast({property_name} as {{{{ dbt.type_float()}}}}) else 0 end"
 
                                 aggregate_attributes.append(
                                     {
