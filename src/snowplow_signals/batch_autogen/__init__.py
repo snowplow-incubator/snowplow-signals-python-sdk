c2351dc68bcd8cbcc42b5a8d8aebe5f4b7b88cee
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2025-04-23 11:55:56 +0300

    Update name of extra python module

--- src/snowplow_signals/batch_autogen/__init__.py
+++ src/snowplow_signals/batch_autogen/__init__.py
@@ -8,6 +8,6 @@ except ImportError as e:
     raise ImportError(
         "The batch project auto-generation requires additional dependencies. "
         "Install them with either:\n"
-        "  pip install 'snowplow-signals[dbt]'\n"
-        "  poetry install --extras dbt"
+        "  pip install 'snowplow-signals[batch-engine]'\n"
+        "  poetry install --extras batch-engine"
     ) from e
