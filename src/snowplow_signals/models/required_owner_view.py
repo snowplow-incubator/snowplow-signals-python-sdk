942e5738c164f46a9d77351b68a6511c14fcf5e2
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2025-05-13 17:17:52 +0300

    Validate to be email

--- src/snowplow_signals/models/required_owner_view.py
+++ src/snowplow_signals/models/required_owner_view.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, EmailStr
 from .view import View
 
 
@@ -7,7 +7,7 @@ class RequiredOwnerView(View):
     A View class that requires the owner field to be set.
     This is used specifically for view creation to enforce owner attribution.
     """
-    owner: str = Field(
+    owner: EmailStr = Field(
         ...,
         description="The owner of the view, typically the email of the primary maintainer. This field is required for view creation.",
         title="Owner",
