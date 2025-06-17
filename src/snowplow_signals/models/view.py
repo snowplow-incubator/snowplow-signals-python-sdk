29cf47c7ec7af2398251b9ee1c648392ea0c9d6a
Jack Keene <jack.keene@snowplowanalytics.com>
2025-06-17 14:14:09 +0100

    add get attributes for view

--- src/snowplow_signals/models/view.py
+++ src/snowplow_signals/models/view.py
@@ -1,10 +1,16 @@
-from pydantic import Field as PydanticField, BeforeValidator, EmailStr
-from typing import Annotated
+from typing import TYPE_CHECKING, Annotated
+
+from pydantic import BeforeValidator, EmailStr
+from pydantic import Field as PydanticField
+
 from .model import (
-    ViewInput,
     Entity,
     LinkEntity,
+    ViewInput,
 )
+
+if TYPE_CHECKING:
+    from snowplow_signals.signals import Signals
 
 
 def entity_to_link(entity: Entity | LinkEntity) -> LinkEntity:
@@ -25,3 +31,23 @@ class View(ViewInput):
         description="The owner of the view, typically the email of the primary maintainer. This field is required for view creation.",
         title="Owner",
     )
+
+    def get_attributes(self, signals: "Signals", identifiers: list[str] | str):
+        """
+        Retrieves the online attributes for this view.
+
+        Args:
+            signals: The Signals instance to use for retrieving attributes.
+            identifiers: The list of entity identifiers to retrieve attributes for.
+
+        Returns:
+            The online attributes for the view.
+        """
+
+        return signals.get_view_attributes(
+            name=self.name,
+            version=self.version,
+            entity=self.entity.name,
+            identifiers=identifiers,
+            attributes=[attribute.name for attribute in self.attributes],
+        )
