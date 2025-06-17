a3ab8adce8aff3bd62444ed88089de062ec42523
Jack Keene <jack.keene@snowplowanalytics.com>
2025-06-17 14:19:31 +0100

    separate service and view get functions

--- src/snowplow_signals/attributes_client.py
+++ src/snowplow_signals/attributes_client.py
@@ -2,12 +2,7 @@ from .api_client import ApiClient
 from .models import (
     GetAttributesResponse,
     GetOnlineAttributesRequest,
-    OnlineAttributesResponse,
-    Service,
-    View,
-    ViewResponse,
 )
-from .registry_client import RegistryClient
 
 
 class AttributesClient:
@@ -16,52 +11,40 @@ class AttributesClient:
 
     def get_view_attributes(
         self,
-        view: View | ViewResponse,
+        name: str,
+        version: int,
+        entity: str,
         identifiers: list[str] | str,
-    ) -> OnlineAttributesResponse:
-        attributes = [
-            f"{view.name}_v{view.version}:{attribute.name}"
-            for attribute in (view.attributes or []) + (view.fields or [])
-        ]
+        attributes: list[str] | str,
+    ) -> GetAttributesResponse:
+
+        attributes = (
+            [f"{name}_v{version}:{attribute}" for attribute in attributes]
+            if isinstance(attributes, list)
+            else [attributes]
+        )
 
         request = GetOnlineAttributesRequest(
             attributes=attributes,
             entities={
-                view.entity.name: (
+                entity: (
                     identifiers if isinstance(identifiers, list) else [identifiers]
                 )
             },
         )
         return self._make_request(request)
 
-    def _get_entity_name(self, service: Service) -> str:
-        unique_entity_names: set[str] = set()
-        for view in service.views:
-            fetched_view = RegistryClient(api_client=self.api_client).get_view(
-                name=view.name, version=view.version
-            )
-            unique_entity_names.add(fetched_view.entity.name)
-
-        if len(unique_entity_names) > 1:
-            raise ValueError(
-                "The service contains views with different entities which is not supported."
-            )
-
-        return unique_entity_names.pop()
-
     def get_service_attributes(
         self,
-        service: Service,
+        name: str,
+        entity: str,
         identifiers: list[str] | str,
-    ) -> OnlineAttributesResponse:
-        if not service.views:
-            raise ValueError("No views to fetch.")
+    ) -> GetAttributesResponse:
 
-        entity_name = self._get_entity_name(service=service)
         request = GetOnlineAttributesRequest(
-            service=service.name,
+            service=name,
             entities={
-                entity_name: (
+                entity: (
                     identifiers if isinstance(identifiers, list) else [identifiers]
                 )
             },
@@ -70,10 +53,10 @@ class AttributesClient:
 
     def _make_request(
         self, request: GetOnlineAttributesRequest
-    ) -> OnlineAttributesResponse:
+    ) -> GetAttributesResponse:
         response = self.api_client.make_request(
             method="POST",
             endpoint="get-online-attributes",
             data=request.model_dump(mode="json"),
         )
-        return OnlineAttributesResponse(data=response)
+        return GetAttributesResponse(data=response)
