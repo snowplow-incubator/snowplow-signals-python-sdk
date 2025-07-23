905acdb7a3d21a29d2df92d0ea43f271b1388eea
Jack Keene <jack.keene@snowplowanalytics.com>
2025-07-23 16:36:14 +0100

    add return types to test_client_auth

--- test/auth/test_client_auth.py
+++ test/auth/test_client_auth.py
@@ -1,6 +1,11 @@
+from typing import Any
+
 import httpx
 import jwt
 import pytest
+from respx import MockRouter
+
+from snowplow_signals import Signals
 
 from snowplow_signals import Signals
 
@@ -8,13 +13,15 @@ from ..utils import MOCK_ORG_ID, utc_timestamp
 
 
 @pytest.fixture
-def payload():
+def payload() -> dict[str, Any]:
     """Creates a sample JWT claimset for use as a payload during tests"""
     return {"iss": "peter", "exp": utc_timestamp() + 15, "claim": "foo"}
 
 
 @pytest.mark.noauthmock
-def test_client_fetches_and_adds_auth(respx_mock, payload, signals_client: Signals):
+def test_client_fetches_and_adds_auth(
+    respx_mock: MockRouter, payload: dict[str, Any], signals_client: Signals
+):
     jwt_token = jwt.encode(payload, "secret")
     token_request = respx_mock.get(
         f"https://console.snowplowanalytics.com/api/msc/v1/organizations/{MOCK_ORG_ID}/credentials/v3/token"
@@ -40,7 +47,9 @@ def test_client_fetches_and_adds_auth(respx_mock, payload, signals_client: Signa
 
 
 @pytest.mark.noauthmock
-def test_client_api_client_expire_token(respx_mock, payload, signals_client: Signals):
+def test_client_api_client_expire_token(
+    respx_mock: MockRouter, payload: dict[str, Any], signals_client: Signals
+):
     payload["exp"] = utc_timestamp() - 15
     expired_jwt_token = jwt.encode(payload, "secret")
     token_request = respx_mock.get(
