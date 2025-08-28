789fc27028bc8feaee8b2ab0eff7e4a5d4aba912
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2025-08-28 14:56:37 +1000

    PR feedback

--- src/snowplow_signals/interventions_subscription.py
+++ src/snowplow_signals/interventions_subscription.py
@@ -1,5 +1,6 @@
+import atexit
 from collections.abc import Callable, Generator, Iterable, Iterator
-from contextlib import AbstractContextManager
+from contextlib import AbstractContextManager, closing
 from queue import Empty, Queue
 from threading import Event, Thread
 from typing import Self
@@ -22,7 +23,11 @@ class InterventionsSubscription(AbstractContextManager, Iterable):
     With buffering enabled, you can iterate over any buffered interventions via iteration.
     Upon exiting the `with` statement or upon calling `stop()`, aborts the request.
     The same subscription can be restarted if required, but not started multiple times concurrently.
+    When started, subscription instances are registered in the class-level `InterventionSubscription.instances` set.
+    This keeps a reference to the instance to prevent it being garbage collected so it can continue passing interventions to callbacks even when out of scope.
     """
+
+    instances: set[Self] = set()
 
     def __init__(
         self, api_client: ApiClient, targets: EntityIdentifiers, buffer=True
@@ -31,16 +36,22 @@ class InterventionsSubscription(AbstractContextManager, Iterable):
         self.api_client = api_client
         self._queue: Queue[InterventionInstance] | None = Queue() if buffer else None
         self._close: Event = Event()
-        self._handlers: list[Callable[[InterventionInstance], object]] = []
-        self._stream: Generator[str | None] | None = None
+        self._handlers: dict[Callable[[InterventionInstance], object], bool] = {}
+        self._thread: Thread | None = None
 
     def __enter__(self) -> Self:
         self.start()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.stop()
-        return False
+
+    def __del__(self):
+        self.stop()
+
+    @classmethod
+    def cleanup(cls):
+        cls.instances.clear()
 
     def __iter__(self) -> Iterator[InterventionInstance]:
         """
@@ -61,59 +72,93 @@ class InterventionsSubscription(AbstractContextManager, Iterable):
         """
         Send request to the Signals API for interventions, and start a background thread to dispatch them to the local buffer or registered handlers.
         """
-        if self._stream:
+        if self._thread and self._thread.is_alive():
             raise RuntimeError("Subscription already running")
 
+        self.instances.add(self)
         self._close.clear()
 
-        self._stream = self.api_client.make_stream_request(
+        stream = self.api_client.make_stream_request(
             method="GET",
             endpoint="interventions",
             params=self.targets.root,
             headers={"Accept": "text/event-stream"},
         )
 
-        Thread(
+        self._thread = Thread(
             target=self._pipe,
             name=f"SignalsInterventions-{id(self)}",
-            args=(self._stream, self._close),
-        ).start()
+            daemon=True,
+            args=(stream, self._close),
+        )
+
+        self._thread.start()
 
     def get(self, block=True, timeout=None) -> InterventionInstance:
         """
         Get an already retrieved intervention from the buffer, or block waiting for a new intervention to be retrieved and enter the buffer.
         If buffering is disabled, raises a `TypeError`.
+
+        Args:
+            block: If True (the default) block until an intervention is available in the buffer, else only return an already available intervention (or raise `queue.Empty` if none).
+            timeout: If `block` is True, wait at most `timeout` seconds before failing with `queue.Empty`; if `None`, wait indefinitely. Ignored if `block` is False.
         """
         if self._queue:
             return self._queue.get(block, timeout)
         else:
             raise TypeError("Buffering required to iterate over subscription")
 
-    def add_handler(self, handler: Callable[[InterventionInstance], object]) -> None:
+    def add_handler(
+        self, handler: Callable[[InterventionInstance], object], ignore_fail=True
+    ) -> None:
         """
         Register a new handler to receive interventions.
+
+        Args:
+            handler: The handler function to be added to the subscription.
+            ignore_fail: If True, exceptions thrown by the handler will be swallowed.
         """
-        self._handlers.append(handler)
+        self._handlers.setdefault(handler, ignore_fail)
+
+    def remove_handler(self, handler: Callable[[InterventionInstance], object]) -> bool:
+        """
+        De-register a previously added intervention handler.
+
+        Args:
+            handler: The handler function to be removed from the subscription.
+        Returns:
+            True if the handler was removed; False if the handler was not already registered.
+        """
+        return self._handlers.pop(handler, None) is not None
 
     def stop(self) -> None:
         """
         Abort the request to the API that is awaiting new interventions, closing the thread that forwards them to registered handlers or the local buffer.
         """
         self._close.set()
+        self.instances.discard(self)
 
     def _pipe(self, stream: Generator[str | None], closed: Event) -> None:
-        for event in stream:
-            if closed.is_set():
-                stream.close()
-                break
+        with closing(stream):
+            for event in stream:
+                if closed.is_set():
+                    break
 
-            if event and event.startswith("data: "):
-                intervention = InterventionInstance.model_validate_json(
-                    event[len("data: ") :]
-                )
+                if event and event.startswith("data: "):
+                    intervention = InterventionInstance.model_validate_json(
+                        event[len("data: ") :]
+                    )
 
-                if self._queue:
-                    self._queue.put(intervention)
+                    if self._queue:
+                        self._queue.put(intervention)
 
-                for handler in self._handlers:
-                    handler(intervention)
+                    for handler, ignore_fail in self._handlers.items():
+                        try:
+                            handler(intervention)
+                        except Exception:
+                            if not ignore_fail:
+                                raise
+                            continue
+
+
+atexit.register(InterventionsSubscription.cleanup)
