4a5c1232670fa79aba0c3ae590b7e14a122b6683
Matus Tomlein <matus.tomlein@gmail.com>
2026-02-04 11:17:12 +0100

    Raise an error in case of unexpected prerelease format

--- scripts/version.py
+++ scripts/version.py
@@ -67,8 +67,7 @@ def bump_prerelease(
                 # Same type, increment the number
                 new_prerelease = f"{prefix}{int(num) + 1}"
         else:
-            # Fallback if format is unexpected
-            new_prerelease = f"{prerelease_type}1"
+            raise ValueError("Invalid pre-release format")
 
         new_version = version.replace(prerelease=new_prerelease)
     else:
